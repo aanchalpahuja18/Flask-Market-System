f0e68fc0fd997a53668774cafb5958a042d7b30e
aanchalpahuja18 <aanchalpahuja34@gmail.com>
2024-11-21 15:07:33 +0530

    Day 3 : Sending data to Templates

--- market.py
+++ market.py
@@ -6,6 +6,15 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+
+@app.route('/market')
+def market_page():
+    items = [
+        {'id': 1, 'name': 'Phone', 'barcode': '#893212299897', 'price': 500},
+        {'id': 2, 'name': 'Laptop', 'barcode': '#123985473165', 'price': 900},
+        {'id': 3, 'name': 'Keyboard', 'barcode': '#231985128446', 'price': 150}
+    ]
+    return render_template('market.html', items=items)
 # render_template is a way to accept the requests & redirect into the html files basically it is just a way to connect our route directly to the html files. We have to import it with our Flask module!
 
 # In order to handle 2 routes with our single html page (since we want to display the same in the case of home & in the case of our root url of website) so we just have to define 1 more route which contains the url of '/home'
